db75dd957c4e0d38e528630b2d4fdd108dee0d35
rmejia-tajc <rmejia.tajc@gmail.com>
2019-01-30 14:30:10 -0700

    create action creator that get data from api

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,4 +9,14 @@ export const FETCH_CHARS_FAILURE = "FETCH_CHARS_FAILURE";
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
-// remember that now we have controll over our thunk-based action creator
+// remember that now we have control over our thunk-based action creator
+export const getChars = () => {
+    return function(dispatch) {
+        dispatch({ type: FETCH_CHARS_START});
+    
+        axios
+            .get(`https://swapi.co/api/people/`)
+            .then(res => dispatch({ type: FETCH_CHARS_SUCCESS, payload: res.data}))
+            .catch(err => dispatch({ type: FETCH_CHARS_FAILURE, payload: err}))
+    }
+}
